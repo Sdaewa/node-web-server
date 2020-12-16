a3675aa0b72f64bd4fd03b8cbfcde2ef185aea89
Luis Ramirez <sdaewa22@gmail.com>
2020-12-16 15:42:18 +0100

    adding browser functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
-console.log('print');
+const submitBtn = document.getElementById('submitBtn');
+
+
+fetch('http://localhost:3000/weather?address=london').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log('error');
+        } else {
+            console.log(data);
+        };
+    });
+});
