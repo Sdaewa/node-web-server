74229b1cfecde850a3d0ed2c71d6b589c93b2f8e
Luis Ramirez <sdaewa22@gmail.com>
2020-12-17 12:13:22 +0100

    fetch on click

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,21 @@
 const weatherForm = document.querySelector('form');
+const input = document.querySelector('input');
 
 
 
-fetch(`http://localhost:3000/weather?address=!`).then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log('error');
-        } else {
-            console.log(data);
-        };
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    const location = input.value;
+    e.preventDefault();
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log('error');
+            } else {
+                console.log(data);
+            };
+        });
     });
-});
-
-
-weatherForm.addEventListener('submit', () => {
-    console.log('testis');
 });
