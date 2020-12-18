5e8e13dee3afbfde6b046edb21f9124d48b430b4
Luis Ramirez <sdaewa22@gmail.com>
2020-12-18 11:35:42 +0100

    ?

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,27 @@
 const weatherForm = document.querySelector('form');
 const input = document.querySelector('input');
-
-
-
+const msgOne = document.getElementById('message-one');
+const msgTwo = document.getElementById('message-two');
 
 
 
 weatherForm.addEventListener('submit', (e) => {
-    const location = input.value;
     e.preventDefault();
+    const location = input.value;
+    if (!location) {
+        return console.log('Provide address');
+    }
+    const loading = document.createElement('p');
+    loading.textContent = 'Loading';
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log('error');
+                msgOne.textContent = '';
+                msgTwo.textContent = 'Something went wrong';
             } else {
+                msgTwo.textContent = '';
+                msgOne.textContent = document.createElement('h2');
+                msgOne.append(data);
                 console.log(data);
             };
         });
