24c22bd14371ecaeafedf9fe0ccd52fc812a8555
Luis Ramirez <sdaewa22@gmail.com>
2020-12-19 18:57:30 +0100

    bug solved

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,23 @@
 const weatherForm = document.querySelector('form');
 const input = document.querySelector('input');
-const msgOne = document.getElementById('message-one');
-const msgTwo = document.getElementById('message-two');
+const msgOne = document.querySelector('#message-one');
+const msgTwo = document.querySelector('#message-two');
 
 
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = input.value;
-    if (!location) {
-        return console.log('Provide address');
-    }
-    const loading = document.createElement('p');
-    loading.textContent = 'Loading';
+
+    msgOne.textContent = 'Loading...';
+    msgTwo.textContent = '';
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                msgOne.textContent = '';
-                msgTwo.textContent = 'Something went wrong';
+                msgOne.textContent = data.error;
             } else {
-                msgTwo.textContent = '';
-                msgOne.textContent = document.createElement('h2');
-                msgOne.append(data);
-                console.log(data);
+                msgOne.textContent = data.Forecast;
+                msgTwo.textContent = data.Location;
             };
         });
     });
